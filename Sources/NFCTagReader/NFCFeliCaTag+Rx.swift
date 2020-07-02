8bd0e6c97f4e0b98f5e640578a512bb08fbc057d
Karibash <y.karibayashi@gmail.com>
2020-07-02 13:53:56 +0900

    Add FeliCa tag polling commands

--- Sources/NFCTagReader/NFCFeliCaTag+Rx.swift
+++ Sources/NFCTagReader/NFCFeliCaTag+Rx.swift
@@ -10,5 +10,34 @@ import CoreNFC
 import RxSwift
 
 @available(iOS 13.0, *)
+public typealias RxNFCFelicaPollingResponse = (manufactureParameter: Data, requestData: Data)
+
+@available(iOS 13.0, *)
 extension ObservableType where Element == NFCFeliCaTag {
+    
+    // MARK: - Actions -
+    
+    public func polling(
+        systemCode: Data,
+        requestCode: PollingRequestCode,
+        timeSlot: PollingTimeSlot
+    ) -> Observable<RxNFCFelicaPollingResponse> {
+        flatMap { tag in
+            Single.create { observer in
+                tag.polling(
+                    systemCode: systemCode,
+                    requestCode: requestCode,
+                    timeSlot: timeSlot
+                ) { manufactureParameter ,requestData ,error in
+                    if error != nil {
+                        observer(.error(error!))
+                    } else {
+                        observer(.success((manufactureParameter, requestData)))
+                    }
+                }
+                return Disposables.create()
+            }
+        }
+    }
+    
 }
