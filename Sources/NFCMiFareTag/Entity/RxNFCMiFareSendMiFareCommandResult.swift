f3837c411ec3d47364f7c376dbd60a9247e513e0
Karibash <y.karibayashi@gmail.com>
2020-07-21 13:32:39 +0900

    Add a command to send commands to MiFare tags

---
+++ Sources/NFCMiFareTag/Entity/RxNFCMiFareSendMiFareCommandResult.swift
@@ -0,0 +1,14 @@
+//
+//  RxNFCMiFareSendMiFareCommandResult.swift
+//  RxCoreNFC
+//
+//  Created by Karibash on 2020/07/20.
+//  Copyright © 2020 Karibash. All rights reserved.
+//
+
+import Foundation
+
+@available(iOS 13.0, *)
+public struct RxNFCMiFareSendMiFareCommandResult {
+    let data: Data
+}
